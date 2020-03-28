be2f3e759d68400e3ced8147715ad662a588d6b9
Johnny N <johnny.a.nigh@gmail.com>
2020-03-27 22:50:39 -0500

    update: Add a "hello, world" application using  the Python webframework Flask

---
+++ flask/hello_world_flask.py
@@ -0,0 +1,8 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "Flask")
+    return f'Hello, world from {escape(name)}!'
